16fe1e1d346d1076dc11473ed52b4e092c021229
jwink83 <juergen.winkler@xidras.com>
2013-04-08 16:42:15 +0000

    added messaeg

---
+++ cookbooks/jw_cookbook/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: jw_cookbook
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+log "hello try Chef with right scale"
